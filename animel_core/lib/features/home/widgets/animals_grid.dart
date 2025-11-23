c4e292f517eead90a20fb5f81ce3f11751dd91c3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-24 00:06:01 +0200

    Update animals_grid.dart

--- animel_core/lib/features/home/widgets/animals_grid.dart
+++ animel_core/lib/features/home/widgets/animals_grid.dart
@@ -1,3 +1,4 @@
+import 'package:animel_core/features/home/data/animal_demo.dart';
 import 'package:flutter/material.dart';
 import 'animal_card.dart';
 
@@ -9,7 +10,7 @@ class AnimalsGrid extends StatelessWidget {
       name: 'Luciffar',
       imageUrl:
           'https://images.pexels.com/photos/127409/pexels-photo-127409.jpeg',
-      location: 'Cairo, New City',
+      location: 'Cairo, New City, 11th St...',
       timeAgo: '13 days ago',
       status: null,
     ),
@@ -17,9 +18,57 @@ class AnimalsGrid extends StatelessWidget {
       name: 'Luciffar',
       imageUrl:
           'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg',
-      location: 'Cairo, New City',
+      location: 'Cairo, New City, 11th St...',
       timeAgo: '10 days ago',
       status: 'Already Home',
+    ),
+    AnimalDemo(
+      name: 'Snow',
+      imageUrl:
+          'https://images.pexels.com/photos/4587995/pexels-photo-4587995.jpeg',
+      location: 'Giza, 6th of October',
+      timeAgo: '5 days ago',
+      status: null,
+    ),
+    AnimalDemo(
+      name: 'Rocky',
+      imageUrl:
+          'https://images.pexels.com/photos/5731862/pexels-photo-5731862.jpeg',
+      location: 'Alexandria, Miami',
+      timeAgo: '2 days ago',
+      status: null,
+    ),
+    AnimalDemo(
+      name: 'Milo',
+      imageUrl:
+          'https://images.pexels.com/photos/1390361/pexels-photo-1390361.jpeg',
+      location: 'Cairo Downtown',
+      timeAgo: '1 day ago',
+      status: null,
+    ),
+    AnimalDemo(
+      name: 'Bella',
+      imageUrl:
+          'https://images.pexels.com/photos/208984/pexels-photo-208984.jpeg',
+      location: 'Nasr City',
+      timeAgo: '7 days ago',
+      status: 'Already Home',
+    ),
+    AnimalDemo(
+      name: 'Shadow',
+      imageUrl:
+          'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg',
+      location: 'Giza – Haram',
+      timeAgo: '3 days ago',
+      status: null,
+    ),
+    AnimalDemo(
+      name: 'Oreo',
+      imageUrl:
+          'https://images.pexels.com/photos/236606/pexels-photo-236606.jpeg',
+      location: 'Alex – Smouha',
+      timeAgo: '12 days ago',
+      status: null,
     ),
   ];
 
@@ -33,22 +82,12 @@ class AnimalsGrid extends StatelessWidget {
         crossAxisCount: 2,
         mainAxisSpacing: 14,
         crossAxisSpacing: 14,
-        childAspectRatio: 0.75,
+        childAspectRatio: 0.68,
       ),
-      itemBuilder: (context, i) => AnimalCard(data: animals[i]),
+      itemBuilder: (context, index) {
+        final item = animals[index];
+        return AnimalCard(data: item);
+      },
     );
   }
-}
-
-class AnimalDemo {
-  final String name, imageUrl, location, timeAgo;
-  final String? status;
-
-  const AnimalDemo({
-    required this.name,
-    required this.imageUrl,
-    required this.location,
-    required this.timeAgo,
-    this.status,
-  });
 }
