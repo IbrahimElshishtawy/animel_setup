d0b05bb24abc0ee837053142526e45131d21bf44
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-24 19:18:50 +0200

    Update account_header_icon.dart

--- animel_core/lib/features/profile/widgets/account_header_icon.dart
+++ animel_core/lib/features/profile/widgets/account_header_icon.dart
@@ -7,18 +7,18 @@ class AccountHeaderIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: CircleAvatar(
-        radius: 44,
+        radius: 90,
         backgroundColor: const Color(0xFFF6ECF3),
         child: Container(
-          width: 70,
-          height: 70,
+          width: 160,
+          height: 200,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            border: Border.all(width: 3, color: const Color(0xFF4B1A45)),
+            border: Border.all(width: 5, color: const Color(0xFF4B1A45)),
           ),
           child: const Icon(
             Icons.person_outline,
-            size: 40,
+            size: 100,
             color: Color(0xFF4B1A45),
           ),
         ),
