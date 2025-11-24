60af163846bd875821c93fe6a63e1763bf78e7e3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-24 18:16:35 +0200

    Update splash_screen.dart

--- animel_core/lib/features/splash/presentation/splash_screen.dart
+++ animel_core/lib/features/splash/presentation/splash_screen.dart
@@ -25,10 +25,21 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(Icons.pets, size: 80, color: Color(0xFF4B1A45)),
-            SizedBox(height: 16),
-            Text(
+          children: [
+            // Icon(Icons.pets, size: 80, color: Color(0xFF4B1A45)),
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              child: SizedBox(
+                height: 140,
+                width: double.infinity,
+                child: Image.asset('assets/image/image.png'),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
               "HopePaw",
               style: TextStyle(
                 fontSize: 26,
